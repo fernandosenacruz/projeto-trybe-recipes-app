605190c8c91745392b6e41d317141df4219bd1c7
Fernando Sena <fernandosenacruz@gmail.com>
2022-01-04 19:01:26 -0300

    requisito 50 feito porem nao passa no teste

--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -1,14 +1,16 @@
 import PropTypes from 'prop-types';
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import { getIngredients, getMeasures } from '../helpers/getIngredients';
+import checkIngredients from '../helpers/checkIngredients';
 
 const IngredientsList = ({ recipe }) => {
   const location = useLocation();
+  const { id } = useParams();
   const [ingredientsList, setIngredientsList] = useState([]);
   const [measuresList, setMeasureList] = useState([]);
   const [isProgressRoute, setIsProgressRoute] = useState(false);
-  const [isDone, setIsDone] = useState({});
+  const [isDone, setIsDone] = useState([]);
 
   useEffect(() => {
     const inProgress = location.pathname.includes('in-progress');
@@ -17,15 +19,26 @@ const IngredientsList = ({ recipe }) => {
     setIngredientsList(ingredientList);
     setMeasureList(measureList);
     setIsProgressRoute(inProgress);
-  }, [recipe]);
+  }, [location, recipe]);
+
+  useEffect(() => {
+    const inProgressRecipes = JSON.parse(localStorage.getItem('inProgressRecipes')) || [];
+    if (inProgressRecipes.length !== 0) setIsDone(...inProgressRecipes.cocktails[id]);
+  }, [id]);
+
+  useEffect(() => console.log(isDone), [isDone]);
 
   const handleChange = ({ target }) => {
-    console.log(target.value);
+    let completedIngredients = {};
     if (target.value === 'true') {
+      completedIngredients = { ...isDone, [target.name]: false };
       setIsDone({ ...isDone, [target.name]: false });
     } else {
+      completedIngredients = { ...isDone, [target.name]: true };
       setIsDone({ ...isDone, [target.name]: true });
     }
+    const favoritedRecipe = { [id]: [completedIngredients] };
+    checkIngredients('cocktails', favoritedRecipe);
   };
 
   return (
@@ -46,6 +59,8 @@ const IngredientsList = ({ recipe }) => {
               id=""
               onChange={ handleChange }
               value={ isDone[`checkbox${index}`] || false }
+              checked={ isDone[`checkbox${index}`] || false }
+              data-testid={ `${index}-ingredient-step` }
             />
             {`${item}: ${measuresList[index]}`}
           </li>
